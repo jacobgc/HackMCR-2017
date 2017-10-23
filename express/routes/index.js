14427bc023f026505de3331a04c0d8c8f6059ae2
Jacob <jacob.clarke2@gmail.com>
2017-10-23 11:23:17 +0100

    added a jumbotron on the index and created an events page

--- express/routes/index.js
+++ express/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/events', function(req, res, next) {
+    res.render('events', { title: 'Express' });
 });
 
 module.exports = router;
